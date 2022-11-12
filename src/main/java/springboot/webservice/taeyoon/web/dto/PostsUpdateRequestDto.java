95ddaf58bd0ddcd085332dfa529c297ce123948a
taeyoon-cloud <xodbs448@hanyang.ac.kr>
2022-11-12 17:59:18 +0900

    4.3 - 게시글 등록 화면 만들기

--- src/main/java/springboot/webservice/taeyoon/web/dto/PostsUpdateRequestDto.java
+++ src/main/java/springboot/webservice/taeyoon/web/dto/PostsUpdateRequestDto.java
@@ -11,7 +11,6 @@ public class PostsUpdateRequestDto {
     private String content;
 
     @Builder
-
     public PostsUpdateRequestDto(String title, String content) {
         this.title = title;
         this.content = content;
