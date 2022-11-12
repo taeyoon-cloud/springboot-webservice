16abaf24a319fef98ff7119bc8b06ed8050b851d
taeyoon-cloud <xodbs448@hanyang.ac.kr>
2022-11-12 18:22:03 +0900

    4.4 - 전체 조회 화면 만들기

--- src/main/java/springboot/webservice/taeyoon/service/posts/PostsService.java
+++ src/main/java/springboot/webservice/taeyoon/service/posts/PostsService.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import springboot.webservice.taeyoon.domain.posts.Posts;
 import springboot.webservice.taeyoon.domain.posts.PostsRepository;
+import springboot.webservice.taeyoon.web.dto.PostsListResponseDto;
 import springboot.webservice.taeyoon.web.dto.PostsResponseDto;
 import springboot.webservice.taeyoon.web.dto.PostsSaveRequestDto;
 import springboot.webservice.taeyoon.web.dto.PostsUpdateRequestDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,7 +35,14 @@ public class PostsService {
 
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
-                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = "+ id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/springboot/webservice/taeyoon/web/IndexController.java
+++ src/main/java/springboot/webservice/taeyoon/web/IndexController.java
@@ -2,14 +2,18 @@ package springboot.webservice.taeyoon.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import springboot.webservice.taeyoon.service.posts.PostsService;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
