835b0d88bec0d71da2b5e41ce7ccaac94ecd97f6
taeyoon-cloud <xodbs448@hanyang.ac.kr>
2022-11-12 15:04:40 +0900

    스프링 부트와 AWS로 혼자 구현하는 웹 서비스 ~3.3장

---
+++ src/main/java/springboot/webservice/taeyoon/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package springboot.webservice.taeyoon.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
