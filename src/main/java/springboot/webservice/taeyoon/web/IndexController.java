b08dc12006ba5f436d0422171dc8ac1cd16cef71
taeyoon-cloud <xodbs448@hanyang.ac.kr>
2022-11-21 14:33:56 +0900

    5.3 구글 로그인 연동하기

--- src/main/java/springboot/webservice/taeyoon/web/IndexController.java
+++ src/main/java/springboot/webservice/taeyoon/web/IndexController.java
@@ -5,17 +5,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import springboot.webservice.taeyoon.config.auth.dto.SessionUser;
 import springboot.webservice.taeyoon.service.posts.PostsService;
 import springboot.webservice.taeyoon.web.dto.PostsResponseDto;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
