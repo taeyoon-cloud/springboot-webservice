a2ae69d65fb9c8b128bcc720eacb8d7a9c1b8eb1
taeyoon-cloud <xodbs448@hanyang.ac.kr>
2023-01-04 15:32:38 +0900

    Application.java에 @EnableJpaAuditing 추가

--- src/main/java/springboot/webservice/taeyoon/Application.java
+++ src/main/java/springboot/webservice/taeyoon/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
